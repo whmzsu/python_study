d85ec610c3bce712238f8a86373e40d8796db0da
whmzsu@gmail.com <whmzsu@gmail.com>
2018-03-03 13:25:34 +0800

    test python

---
+++ study1.py
@@ -0,0 +1,8 @@
+from urllib import request
+
+a=1
+b=0
+while a <= 100:
+    b=b+a
+    a=a+1
+print(b)
---
+++ study2.py
@@ -0,0 +1,21 @@
+from urllib import request
+
+
+req = request.Request('http://www.douban.com/')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
+
+import json
+def fetch_data(url):
+    with request.urlopen(url) as f:
+        return json.loads(f.read().decode('utf-8'))
+
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
